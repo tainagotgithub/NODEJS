d5f6bd6c30e45675a53b2a8645dd4a82d0e4ac45
Tainá Puja <84106003+tainagotgithub@users.noreply.github.com>
2022-10-24 21:06:10 -0300

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "server" {
+    ami = var.ami
+    instance_type = var.instance_type
+
+    tags = {
+        Name = var.name
+        Environment = var.env 
+        Provisioner = "Terraform" 
+        Source = var.src
+    }
+  
+}
